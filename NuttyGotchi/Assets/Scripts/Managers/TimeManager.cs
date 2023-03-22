ccf70b247dc3de1a06bfe2e0c2ba745c8a0dc3a4
DarioSpaziani <dario.spaziani94@gmail.com>
2023-03-22 15:43:19 +0100

    Added time manager
    detect the time elapsed and when rich a certain time get start an event. Added a function that detect when the game is paused too.

---
+++ NuttyGotchi/Assets/Scripts/Managers/TimeManager.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+using Utils;
+
+public class TimeManager : Singleton<TimeManager> {
+    public float minForTimeEvent = 1;
+    
+    public delegate void TimeEvent();
+
+    public event TimeEvent ClockTick;
+
+    private DateTime lastTimeDetected = DateTime.Now;
+
+    private float elapsedTime = 0;
+
+    private void Update() {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime >= 60 * minForTimeEvent) {
+            elapsedTime = 0;
+            if (ClockTick != null) {
+                ClockTick();
+            }
+        }
+    }
+    
+    //dato che è per mobile bisogna tenere traccia di quando il giocatore metterà in pausa l'app
+
+    private void OnApplicationPause(bool pauseStatus) {
+        if (pauseStatus) {
+            lastTimeDetected = DateTime.Now;
+        }
+        else {
+            elapsedTime += TimeUtils.differenceInSeconds(lastTimeDetected, DateTime.Now);
+        }
+    }
+}
